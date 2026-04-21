6b00ed79d3a61c3cab405b508add283f2bbc0287
Ruhul Amin <itsruhul@proton.me>
2026-04-21 16:51:35 +0530

    Add Theatre Square [A] from Codeforces

---
+++ 1/A - Theatre Square/solution.cpp
@@ -0,0 +1,1 @@
+#include<bits/stdc++.h>using namespace std; int main() {  long long m, n, a;  cin >> m >> n >> a;    long long x, y;    if (m % a != 0) {      x = (m / a) + 1;  }  else {      x = m / a;  }    if (n % a != 0){      y = (n / a) + 1;  }  else {      y = n / a;  }    cout << x * y << endl;   return 0;}
