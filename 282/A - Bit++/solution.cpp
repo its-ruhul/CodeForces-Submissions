3f6dff005c91337a9d0aa904ed6579b91a8957cc
Ruhul Amin <itsruhul@proton.me>
2026-04-21 13:05:01 +0530

    Add Bit++ [A] from Codeforces

---
+++ 282/A - Bit++/solution.cpp
@@ -0,0 +1,1 @@
+#include<bits/stdc++.h>using namespace std; int main() {  int t;  cin >> t;  int x = 0;   for(int i = 0; i < t; i++){        string s;    cin >> s;        if (s.find("++") != -1){        x++;    }    else if(s.find("--") !=  -1){        x--;    }  }    cout << x << endl;   return 0;}
