81d655bfbe2031d26c44c4e319ecfdc742bda654
Ruhul Amin <itsruhul@proton.me>
2026-04-30 22:16:26 +0530

    Add Even Odds [A] from Codeforces

---
+++ 318/A - Even Odds/solution.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  
+  long long n;
+  long long k;
+  
+  cin >> n >> k;
+  
+  if(k <= (n+1)/2){
+      cout << (2 * k) - 1 << endl;
+  }
+  else {
+      cout << (k - (n+1)/2) * 2 << endl;
+  }
+  
+}
