9e9e9e4f1c5c3d144fe3308722aaac737106b243
Ruhul Amin <itsruhul@proton.me>
2026-05-06 19:10:53 +0530

    Add Expression [A] from Codeforces

---
+++ 479/A - Expression/solution.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  
+    int a, b, c;
+    int ans;
+    
+    cin >> a >> b >> c;
+    
+    ans = a + b + c;
+    
+    ans = max(ans, a * (b + c));
+    ans = max(ans, (a + b) * c);
+    ans = max(ans, a * b * c);
+    
+    cout << ans << endl;
+}
