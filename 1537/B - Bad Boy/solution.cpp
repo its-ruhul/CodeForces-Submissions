d5538439cc861329079cf8d851964d373e1de26a
Ruhul Amin <itsruhul@proton.me>
2026-09-07 19:25:37 +0530

    Add Bad Boy [B] from Codeforces

---
+++ 1537/B - Bad Boy/solution.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int solve(){
+    
+    long long n, m, i, j;
+    cin >> n >> m >> i >> j;
+    
+    cout << 1 << " " << 1 << " ";
+    cout << n << " " << m << endl;
+    
+    return 0;
+}
+ 
+ 
+int main() {
+  
+    int t;
+    cin >> t;
+    
+    for(int i = 0; i < t; i++){
+        solve();
+    }
+    
+    return 0;
+}
